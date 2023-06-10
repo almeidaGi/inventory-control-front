bd0d7ac7d10198be236d6913ce584b3cc5b3d809
rod-sousa <rod.sousa899@gmail.com>
2023-06-09 21:09:16 -0300

    Created component help

---
+++ src/app/shared/components/help/help.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-app-help',
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.scss']
+})
+export class HelpComponent implements OnInit {
+
+  constructor(private router: Router
+     ) { }
+
+  ngOnInit(): void {
+  }
+
+  public backToProductList() {
+    this.router.navigate(['/home']);
+  }
+}
